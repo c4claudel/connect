d4d29d490acfa540e8839ab43653146467344fb7
Tim Edmonds <tim@gnat.localdomain>
2024-12-21 17:38:00 +0100

    connect #33

--- mailer.py
+++ mailer.py
@@ -2,7 +2,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-#from googleapiclient.errors import HttpError
+from googleapiclient.errors import HttpError
 
 from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
